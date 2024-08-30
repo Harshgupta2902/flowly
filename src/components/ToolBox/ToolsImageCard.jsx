046ed2e46acf977dbe90cf0381f3acf48d513c44
Harsh <harsh.gupta@cognustechnology.com>
2024-08-30 16:39:23 +0530

    QUERIES CHANGES

--- src/components/ToolBox/ToolsImageCard.jsx
+++ src/components/ToolBox/ToolsImageCard.jsx
@@ -1,85 +1,19 @@
-import Image from "next/image";
 import Link from "next/link";
 import { sql } from "@vercel/postgres";
 
-const defaultBase64 = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAABLCAQAAAA1k5H2AAAAi0lEQVR42u3SMQEAAAgDoC251a3gL2SgmfBYBRAA`;
-
-const staticPosts = [
-  // Example data, replace with actual static data
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-  {
-    title: "Tool 1",
-    slug: "tool-1",
-    coverImage:
-      "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    logo: "https://prototyprio.gumlet.io/strapi/b1f1098f2ac161fab1ef44ba445902d4.png",
-    tags: [{ attributes: { name: "Tag1" } }],
-  },
-];
-
 const ToolImageCard = async () => {
-  const { rows } = await sql`SELECT * FROM packages LIMIT 20`;
-  console.log(rows);
+  const { rows } = await sql`
+    SELECT * FROM packages 
+    ORDER BY 
+        (scores->>'popularity')::int DESC,
+        (scores->>'pubPoints')::int DESC,
+        (scores->>'likes')::int DESC
+    LIMIT 30`;
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full flex-wrap">
       {rows.map((post, i) => {
-        const { name, link, coverImage, compatibility, description } = post;
+        const { name, link, compatibility, description } = post;
 
         return (
           <div key={i}>
